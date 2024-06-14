9d4388ce543b175e33566bf47c0c4ae2d31e76d7
JuanJoseLL <1001367985@u.icesi.edu.co>
2024-06-14 09:48:29 -0500

    fix tests and reformat yml file

--- src/auth/services/auth_google.service.ts
+++ src/auth/services/auth_google.service.ts
@@ -3,7 +3,7 @@ import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from "@nestjs/typeorm";
 import { Customer } from "../../users/entities/customer.entity";
 import { Repository } from "typeorm";
-import { CreateCustomerDto } from "src/users/dto/create-customer.dto";
+import { CreateCustomerDto } from "../../users/dto/create-customer.dto";
 
 
 
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -8,7 +10,12 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {}, // Provide a mock implementation if needed
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
