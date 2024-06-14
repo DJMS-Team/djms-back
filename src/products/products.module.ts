294c50d4755c88a045835a50d8b15857267d58b8
Mateo Silva <1000035385@u.icesi.edu.co>
2024-06-14 13:07:59 -0500

    hotfix changes

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,18 +3,26 @@ import { AuthModule } from 'src/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { Product } from './entities/products.entity';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../users/users.service';
 import { ProductsController } from './controllers/products.controller';
 import { ProductsService } from './services/products.service';
 import { ProductCategoryService } from './services/product-category.service';
+import { User } from '../users/entities/user.entity';
+import { OrderDetail } from '../orders/entities/order_detail.entity';
+import { Review } from '../resources/entities/review.entity';
+import { ProductCategory } from './entities/product-category.entity';
+import { Inventory } from '../inventories/entities/inventory.entity';
+import { Comment } from '../resources/entities/comment.entity';
+import { Repository } from 'typeorm';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService, UsersService, ProductCategoryService],
-  exports: [ProductsModule, TypeOrmModule],
+  providers: [ProductsService, UsersService, ProductCategoryService, Repository],
+  exports: [ProductsModule, TypeOrmModule, Repository],
   imports: [
-    TypeOrmModule.forFeature([Product]), 
-    ConfigModule
+    TypeOrmModule.forFeature([Product, User, OrderDetail, Review, ProductCategory, Inventory, Comment]), 
+    ConfigModule, 
+    Repository
   ]
 })
 export class ProductsModule {}
