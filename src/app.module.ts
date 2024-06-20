d62a94f5b101d59d5ef542ef504c70ad2df398e9
dylanbc1 <dyez1110@gmail.com>
2024-06-20 13:47:58 -0500

    fix: Change e2e test

--- src/app.module.ts
+++ src/app.module.ts
@@ -30,8 +30,8 @@ import { ReportsModule } from './reports/reports.module';
       ssl: {
         rejectUnauthorized :false
       },
-      logger: 'advanced-console',
-      logging: 'all'
+      //logger: 'advanced-console',
+      //logging: 'all'
     }),
     AuthModule,
     ResourcesModule,
