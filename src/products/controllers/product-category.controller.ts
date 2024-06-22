32942c62ac543c92bdf1ca51de8d7866db1142cd
dylanbc1 <dyez1110@gmail.com>
2024-06-22 01:12:46 -0500

    feat: Add Roles and Auth guards in create product category

--- src/products/controllers/product-category.controller.ts
+++ src/products/controllers/product-category.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ProductCategoryService } from '../services/product-category.service';
 import { CreateProductCategoryDto } from '../dto/create-product-category.dto';
+import { Roles } from 'src/auth/decorators/role.decorator';
+import { Role } from 'src/users/entities/roles.enum';
+import { AuthGuard } from '../../auth/guard/auth.guard';
+import { RolesGuard } from '../../auth/guard/roles.guard';
 
 @Controller('product-category')
 export class ProductCategoryController {
   constructor(private readonly productCategoryService: ProductCategoryService) {}
 
   @Post()
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
   create(@Body() createProductCategoryDto: CreateProductCategoryDto) {
     return this.productCategoryService.create(createProductCategoryDto);
   }
