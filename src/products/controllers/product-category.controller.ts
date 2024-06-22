b3a61a93bab696412db6f6ce141eef68211015ee
dylanbc1 <dyez1110@gmail.com>
2024-06-22 01:15:12 -0500

    fix: Change imports in product category

--- src/products/controllers/product-category.controller.ts
+++ src/products/controllers/product-category.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ProductCategoryService } from '../services/product-category.service';
 import { CreateProductCategoryDto } from '../dto/create-product-category.dto';
-import { Roles } from 'src/auth/decorators/role.decorator';
-import { Role } from 'src/users/entities/roles.enum';
+import { Roles } from '../../auth/decorators/role.decorator';
+import { Role } from '../../users/entities/roles.enum';
 import { AuthGuard } from '../../auth/guard/auth.guard';
 import { RolesGuard } from '../../auth/guard/roles.guard';
 
